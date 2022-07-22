a63dfcf89cfa0b505832ee5f910bc900f13a9bc4
KripaVS <kripa.ev@inapp.com>
2022-07-22 16:21:36 +0530

    added more files

---
+++ week4.py
@@ -0,0 +1,9 @@
+#file handling
+files=open("myfile.txt","r")#read
+#print(files.read())#reads the whole file
+
+#reading specified no of char
+#print(files.read(10))#Jack and J
+
+#reading a line frm txtfile
+print(files.readline())#Jack and Jill 
