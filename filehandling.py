3615438d1b20225e01cd9bbf1ca54cb6f532d3a9
KripaEV <109217428+KripaEV@users.noreply.github.com>
2022-07-26 10:53:51 +0530

    Update filehandling.py

--- filehandling.py
+++ filehandling.py
@@ -1,117 +1,53 @@
 import os
-result = os.listdir(os.getcwd())
-print(result)
-
-#os.mkdir("Contacts")
-
-if os.path.exists("contacts.txt"):
-    print("File 'contacts.txt' exists")
-else:
-    file=open("contactsfile.txt","a+")
-    print("File successfully created")
-    file.close()
-
-#fcont=dict()
-
-def listcontacts():
-    file=open("contacts.txt","r")
-    filecontentslist=file.readlines()
-    filecontentslist.sort()
-    print(filecontentslist)
-    file.close()
-
-def addcontact():
-    file=open("contacts.txt","a")
-    filecontact=input("Enter the name and number:")
-    file.write(filecontact)
-    print("Contact successfully added")
-    file.write("\n")
-    file.close()
-
-def namesearch():
-    name=input("Enter the name of contact to search:")
-
-
-#def nosearch():
-
-def deletecontact():
-    file=open("contacts.txt","r")
-    file2=open("temp.txt","w")
-    name=input("Enter the name of contact to delete:")
-    file.write(name)
-    for line in file:
-            if line.strip("\n").startswith(name):
-                file2.write(line)
-    print("Contact successfully deleted")
-    os.remove("temp.txt")
-    file2.close()
-    file.close()
-    
-
-st=1
-
-while(st==1):
-    print('''1.List all contacts
-2.Add a new contact
-3.Search a new contact
-4.Search by number
-5.Delete a contact''')
-    ch=int(input("Enter your choice:"))
-    match(ch):
-        case 1:listcontacts()
-        case 2:addcontact()
-        case 3:namesearch()
-        #case 4:nosearch()
-        #case 5:deletecontact()
-
-
-import os
 import re
 
 #to list all contacts in alphabetical order
-def listcontacts():
-    list1=[]
-    list2=[]
+def list():
+    lst=[]
+    newLst=[]
     try:
-        file=open("contacts\contacts.txt", 'r')
-        for line in file:
-            list1.append(line)
-        for i in list1:
+        f=open('contacts\contacts.txt', 'r')
+        for line in f:
+            lst.append(line)
+        for i in lst:
             if i != 'Name - Phone Number\n':
-                list2.append(i.strip())
-        list2.sort()
-        for i in list2:
+                newLst.append(i.strip())
+        newLst.sort()
+        for i in newLst:
             print(i)
     except:
-        print("Error")
+        print("Unsuccessful")
 
 def addcontact():
     name=input("Enter the name of new contact: ")
     num=input("Enter the phone number: ")
     try:
         file=open("contacts\contacts.txt", "a")
-        filecontactlist= name + " - " + num
-        f.write(filecontactlist)
-        f.write("\n")
-        f.close()
+        filecontactlist=name + " - " + num
+        file.write(filecontactlist)
+        file.write("\n")
+        file.close()
         print("Added Successfully")
     except:
         print("Unsuccessful")
     print("\nUpdated PhoneBook is: ")
-    list()
+    print(list())
 
 
 def deletecontact():
     flag=0
     name = input("Enter the name of contact to delete: ")
     try:
-        file1=open("contacts\contacts.txt", "r")
+        file1=open("contacts\contacts.txt","r")
         line=file1.readlines()
     
         file2=open('contacts\contacts.txt', 'w')
         for l in line:
-            if l.find(name)==-1:
-                file2.write(l)
+            n,phone=l.strip().split('-')
+            if n.strip()==name:
+                print("Contact deleted successfully")
+                continue
+            file2.write(l)
                     
     except:
         print("Unsuccessful")
@@ -121,33 +57,40 @@ def deletecontact():
 
 def searchName():
     name = input("Enter the name of contact to search: ")
+    flag=False
     try:
         file1=open("contacts\contacts.txt", "r")
         line= file1.readlines()
-    
-        file2=open('contacts\contacts.txt', 'r')
         for l in line:
-            if l.find(name) != -1:
-                print('Name - Phone Number\n Contact: ', l)
+            n,phone=l.strip().split('-')
+            if n.strip()==name:
+                print('Name - Phone Number\n', l)
+                print("Contact found")
+                flag=True
+        if not flag:
+            print("Not found")
 
     except:
         print("Unsuccessful")
         
 #to search a contact by number
 def searchno():
+    flag=False
     num = input("Enter the phone number to search: ")
     try:
         file1=open("contacts\contacts.txt", "r")
         line= file1.readlines()
-    
-        file2=open('contacts\contacts.txt', 'r')
         for l in line:
-            if l.find(num) != -1:
-                print('Name - Phone Number\n Contact: ', l)
+            n,phone=l.strip().split('-')
+            if phone.strip()==num:
+                print('Name - Phone Number\n', l)
+                print("Contact found")
+                flag=True
+        if not flag:
+            print("Not found")
     except:
         print("Unsuccessful")
 
-#choice selection
 def accept():   
     while(True):
         ch =int(input("""
