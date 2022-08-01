cb2367272b3926bf6009bac6e46cc6ffdf17eb2d
KripaVS <kripa.ev@inapp.com>
2022-08-01 15:26:38 +0530

    Patient mngt

--- camp2_evaluation/patientmngt.py
+++ camp2_evaluation/patientmngt.py
@@ -1,31 +1,166 @@
 '''Q2. Create a Patient management system python console application that manages records of the patients in a hospital. 
 You may use the MSSQL database'''
 
+from dataclasses import dataclass
 from multiprocessing import connection
 import pyodbc
+import functools
 
 constring='Driver={SQL Server};Server=DESKTOP-9FVJH8C\SQLEXPRESS;Database=PMSystem;Trusted_Connection=yes;'
 
-def initialiseDB():
-    try:
-        print("Connecting to database..")
-        connection = pyodbc.connect(constring)
-        mycursor = connection.cursor()
-        try:     
-            mycursor.execute('''CREATE TABLE phonebook(
-                id INT IDENTITY PRIMARY KEY,
-                name VARCHAR(50) NOT NULL,
-                phone VARCHAR(20) NOT NULL UNIQUE,
-                )''')
+def database_connection(myFunc):
+    @functools.wraps(myFunc)
+    def innerWrapper(*args):
+        try:
+            connection=pyodbc.connect(constring)
+            mycursor=connection.cursor()
+            result = myFunc(mycursor,*args)
+            connection.commit()
+            connection.close()
+            return result
         except Exception as e:
-                print('Something went wrong')
-                print(f"{type(e).__name__} was occured,")
-    except:
-        print("Could not connect to database")
+            print('Connection error')
+            print(e)
+    return innerWrapper
+
+@database_connection
+def allGenderList(mycursor):#gets values from Gender table to display
+    mycursor.execute('SELECT * FROM Gender')
+    gen=mycursor.fetchall()
+    return gen
+
+@database_connection
+def allBloodGroupList(mycursor):#displays values from BloodGroup table
+    mycursor.execute('SELECT * FROM BloodGroup')
+    group=mycursor.fetchall()
+    return group
+
+@database_connection
+def allPatientRecords(mycursor):#gets values from Patient table to display
+    mycursor.execute('SELECT * FROM Patient')
+    patient=mycursor.fetchall()
+    return patient
+
+@database_connection
+def listRecords(mycursor,p_id):
+    while(True):
+        try:
+            mycursor.execute('''SELECT patient_id,patient_name,gender,age,blood_group FROM Patient
+            JOIN Gender ON Patient.gender_no= Gender.gender_no
+            JOIN BloodGroup ON Patient.blood_group_no=BloodGroup.blood_group_no
+            WHERE patient_id=?''',(p_id))
+            #patient=mycursor.fetchall()
+            #print(patient)
+            #status=True
+        except Exception as e:
+                print("Could not search from database")
+                print(e)
+        else:
+            patient=mycursor.fetchall()
+            if len(patient)>0:
+                for patient_id,patient_name,gender_no,age,blood_group_no in patient:
+                    print(f"Patient id: {patient_id}\nPatient name: {patient_name}\nGender: {gender_no}\nAge: {age}\nBlood group: {blood_group_no}")
+            else:
+                print('This number does not exist in database')
+        break
+        
+def addPatientRec():
+    print("Enter the patient details")
+    pat_name=input("Enter the patient name:")
+    gen=allGenderList()
+    for genno,genname in gen:
+        print(genno,genname)
+    gen_name=int(input("Enter the appropriate number of the patients gender:"))
+    Age=int(input("Enter the patient age:"))
+    group=allBloodGroupList()
+    for grpno,grp in group:
+        print(grpno,grp)
+    Group=int(input("Enter the appropriate number of the patients blood group:"))
+        
+    while(True):
+        try:
+            connection = pyodbc.connect(constring)
+            mycursor = connection.cursor()
+            try:
+                mycursor.execute("INSERT INTO Patient(patient_name,gender_no,age,blood_group_no) VALUES (?,?,?,?)",(pat_name,gen_name,Age,Group))           
+            except Exception as e:
+                print("Could not write to database")
+                print(e)
+            else:
+                print("Record addition successful")
+            break
+        except:
+            print("Record addition unsuccessful")
+            continue
     connection.commit()
     connection.close()
 
-def addPatientRec():
+
+def updatePatientRec():
+    op1=1
+    id=int(input("Enter the patient id of the patient to be updated:"))
+    print('''
+    1. Update patient name
+    2. Update patient gender
+    3. Update patient age
+    4. Update patient blood group
+    ''')
+    while(op1==1):
+        updateop=int(input("Enter the field to be updated:"))
+        connection = pyodbc.connect(constring)
+        mycursor = connection.cursor()
+        if updateop==1:
+            pat_name=input("Enter the new patient name:")
+            mycursor.execute("UPDATE Patient SET patient_name=? WHERE patient_id=?",(pat_name,id))
+        elif updateop==2:
+            gen=allGenderList()
+            for genno,genname in gen:
+                print(genno,genname)
+            gen_name=int(input("Enter the appropriate number of the patients gender:"))
+            mycursor.execute("UPDATE Patient SET gender_no=? WHERE patient_id=?",(gen_name,id))
+        elif updateop==3:
+            Age=input("Enter the new patient age:")
+            mycursor.execute("UPDATE Patient SET age=? WHERE patient_id=?",(Age,id))
+        elif updateop==4:
+            group=allBloodGroupList()
+            for grpno,grp in group:
+                print(grpno,grp)
+            Group=int(input("Enter the appropriate number of the patients blood group:"))
+            mycursor.execute("UPDATE Patient SET blood_group_no=? WHERE patient_id=?",(Group,id))
+        else:
+            print("Invalid option")
+            break
+        op1=int(input("Do you want to update again?(1/0):"))
+    connection.commit()
+    connection.close()
+
+def deletePatientRec():
+    p_id=int(input("Enter the patient id of the record to be deleted:"))
+    while(True):
+        try:
+            connection = pyodbc.connect(constring)
+            mycursor = connection.cursor()
+            try:
+                mycursor.execute("DELETE FROM Patient WHERE patient_id=?",(p_id))           
+            except Exception as e:
+                print("Could not delete from database")
+                print(e)
+            else:
+                if mycursor.rowcount>0:
+                    print("Contact deleted successfully")
+                else:
+                    print('This record does not exist in database')
+            break
+        except:
+            print("Record deletion unsuccessful")
+            continue
+    connection.commit()
+    connection.close()
+
+def searchPatientRec():
+    status=False
+    p_id=int(input("Enter the patient id of the record to be searched:"))
+    listRecords(p_id)
     
 
 st=1
@@ -43,5 +178,4 @@ while (st==1):
         case 3: deletePatientRec()
         case 4: searchPatientRec()
         case _: print("Invalid choice")
-
-st=int(input("Do you want to enter another option?(1/0):"))
+    st=int(input("Do you want to enter another option?(1/0):"))
--- camp2_evaluation/patientmngt.spl.sql
+++ camp2_evaluation/patientmngt.spl.sql
@@ -2,3 +2,40 @@ CREATE DATABASE PMSystem
 
 USE PMSystem
 
+CREATE TABLE Gender(
+	gender_no INT IDENTITY PRIMARY KEY,
+	gender VARCHAR(20)
+)
+
+CREATE TABLE BloodGroup(
+	blood_group_no INT IDENTITY PRIMARY KEY,
+	blood_group VARCHAR(5)
+)
+
+CREATE TABLE Patient(
+	patient_id INT NOT NULL PRIMARY KEY IDENTITY,
+	patient_name VARCHAR(20) NOT NULL,
+	gender_no INT NOT NULL FOREIGN KEY REFERENCES Gender(gender_no),
+	age INT NOT NULL,
+	blood_group_no INT NOT NULL FOREIGN KEY REFERENCES BloodGroup(blood_group_no)
+)
+
+INSERT INTO Gender VALUES
+('Male'),
+('Female'),
+('Transgender'),
+('Other')
+
+INSERT INTO BloodGroup VALUES
+('A+'),
+('A-'),
+('B+'),
+('B-'),
+('AB+'),
+('AB-'),
+('O+'),
+('O-')
+
+SELECT * FROM Gender
+SELECT * FROM BloodGroup
+SELECT * FROM Patient
